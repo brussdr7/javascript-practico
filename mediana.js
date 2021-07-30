1dea70234cf44c9093245fdc2bd2f87a80c8bf58
Bruno <brussdr7@gmail.com>
2021-07-30 14:26:18 -0500

    Promedio y media aritmetica

---
+++ mediana.js
@@ -0,0 +1,17 @@
+const lista1 = [10, 20, 3, 345, 48975934];
+
+const mitadLista1 = lista1 / 2;
+
+function esPar(numero) {
+    if(numero % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+if(esPar(lista1.length)) {
+
+} else {
+    mediana = lista1[mitadLista1];
+}
