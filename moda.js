ee88e6463db7ee0c625260c7d952f722898a679b
Bruno <brussdr7@gmail.com>
2021-08-02 14:49:35 -0500

    claculo de la moda completo

--- moda.js
+++ moda.js
@@ -1,13 +1,23 @@
-const lista1 = [1,2,4,2,4,2,1,3,5,3,5,2,6,2,4,,7];
+// const lista1 = [1, 2, 4, 2, 4, 2, 1, 3, 5, 3, 5, 2, 6, 2, 4, , 7];
 
-const listaCount = {};
+function calcularModa(lista) {
 
-lista1.map(
-    function (elemento) {
-        if(listaCount[elemento]) {
+    const listaCount = {};
+
+    lista.map(function (elemento) {
+        if (listaCount[elemento]) {
             listaCount[elemento] += 1;
         } else {
             listaCount[elemento] = 1;
         }
-    }
-);
+    });
+
+    const listaArray = Object.entries(listaCount);
+
+    listaArray.sort(function (elementoA, elementoB) {
+        return elementoA[1] - elementoB[1];
+    });
+
+    const moda = listaArray[listaArray.length - 1];
+    console.log(moda);
+}
