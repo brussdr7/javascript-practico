dbae214209bf4c95102b3db447d26c35eec0acf9
Bruno <brussdr7@gmail.com>
2021-08-04 14:35:58 -0500

    mejorando legibilidad del codigo

--- costosIngresos.js
+++ costosIngresos.js
@@ -4,57 +4,34 @@ const vela = {
     costoFijo: 1000,
 };
 
-const ingresos = [
-    100,
-    200,
-    50,
-    1000,
-    200,
-    5,
-    2,
-    20,
-    100,
-    200
-];
-
-const gastos = [
-    100,
-    20,
-    25,
-    10,
-    10,
-    5,
-    20,
-    20,
-    50,
-    25,
-    100,
-    30,
-    100,
-    25,
-    23,
-    5,
-    200,
-];
-
-// Capacidad de Ahorro
-function obtenerIngresos() {
+function calcularTodo() {
     const listaIngresos = document.getElementById("inputIngresos");
     const valueIngresos = listaIngresos.value;
-
-    let ingresos = Array.from(valueIngresos.split(","), Number);
-    return ingresos;
-}
-
-function obtenerGastos() {
     const listaGastos = document.getElementById("inputGastos");
     const valueGastos = listaGastos.value;
 
+    let ingresos = Array.from(valueIngresos.split(","), Number);
     let gastos = Array.from(valueGastos.split(","), Number);
-    return gastos;
+
+    const ahorro = calcularAhorro(ingresos, gastos);
+    const capEndeuda = calcularCapEndeuda(ingresos, gastos);
+    const montEndeuda = calcularMontEndeuda(ingresos, gastos);
+
+    console.log("tu ahorro es: $" + ahorro);
+    console.log(
+        "tu capacidad de endeudamiento es: "
+        + parseInt(capEndeuda)
+        + "%"
+    );
+    console.log(
+        "tu monto de endeudamiento es: $"
+        + parseInt(montEndeuda)
+    );
 }
 
-function calcularAhorro() {
+// Capacidad de Ahorro
+function calcularAhorro(ingresos, gastos) {
+
     const totalIngresos = ingresos.reduce(function (acumulado = 0, valornuevo) {
         return acumulado + valornuevo;
     });
@@ -62,27 +39,30 @@ function calcularAhorro() {
         return acumulado + valornuevo;
     });
     const ahorro = totalIngresos - gastoTotal;
-    console.log(ahorro);
+    return ahorro;
 }
 
+// Capacidad de Endeudamiento
+function calcularCapEndeuda(ingresos, gastos) {
 
+    const totalIngresos = ingresos.reduce(function (acumulado = 0, valornuevo) {
+        return acumulado + valornuevo;
+    });
+    const gastoTotal = gastos.reduce(function (acumulado = 0, valornuevo) {
+        return acumulado + valornuevo;
+    });
+    const capacidadEndeudamiento = (gastoTotal / totalIngresos) * 100;
+    return capacidadEndeudamiento;
+}
 
+function calcularMontEndeuda(ingresos, gastos) {
 
+    const ahorro = calcularAhorro(ingresos, gastos);
+    const capacidadEndeudamiento = calcularCapEndeuda(ingresos, gastos);
 
-
-// // Capacidad de Endeudamiento
-// const capacidadEndeudamiento = (gastoTotal / totalIngresos) * 100
-// console.log(
-//     "Capacidad Endeudamiento: "
-//     + parseInt(capacidadEndeudamiento)
-//     + "%"
-// );
-
-// const montoEndeudamiento = ahorro * capacidadEndeudamiento;
-// console.log(
-//     "Monto endeudamiento: $"
-//     + parseInt(montoEndeudamiento)
-// );
+    const montoEndeudamiento = ahorro * capacidadEndeudamiento;
+    return montoEndeudamiento;
+}
 
 function calcularPuntoEquilibrio(objeto) {
     resta = objeto.precio - objeto.costoVariable;
