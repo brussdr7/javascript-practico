aaf67f26a6b0c021e567ee26f6f03f1d9a48f24e
Bruno <brussdr7@gmail.com>
2021-08-09 20:09:33 -0500

    implementacion de la interaccion de html con javascript

--- moda.js
+++ moda.js
@@ -1,5 +1,18 @@
 // const lista1 = [1, 2, 4, 2, 4, 2, 1, 3, 5, 3, 5, 2, 6, 2, 4, , 7];
 
+function obtenerData() {
+    const inputLista = document.getElementById("inputLista");
+    const valueLista = inputLista.value;
+
+    const array = Array.from(valueLista.split(","), Number);
+
+    const moda = calcularModa(array);
+    console.log(moda);
+
+    const result = document.getElementById("resultP");
+    result.innerText = "La moda es: " + moda[0] + " con " + moda[1] + " repeticiones";
+}
+
 function calcularModa(lista) {
 
     const listaCount = {};
@@ -19,5 +32,5 @@ function calcularModa(lista) {
     });
 
     const moda = listaArray[listaArray.length - 1];
-    console.log(moda);
+    return moda;
 }
